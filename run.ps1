df13ee547de736ad388b878a245ad0b6f1ad9bc4
relaxhaha56-maker <relaxhaha56@gmail.com>
2026-05-06 04:51:09 +0700

    run.ps1

--- run.ps1
+++ run.ps1
@@ -1,27 +1,48 @@
-$AppScriptUrl = "https://script.google.com/macros/s/AKfycbzV6BvSVGjGQg5afthgYjd-RL5DYtoMqel7QdR85EhpTsPsT1ymtkRs4tylpMig17HAlA/exec"
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$AppScriptUrl = "https://script.google.com/macros/s/AKfycbxaLAAploQ8IiJxcQAzbs6i83XdEuHodQA4gfF9rT3ca2LV4-GJKYBX-9tFq6RUFpN2PA/exec"
 $GitHubRawUrl = "https://raw.githubusercontent.com/relaxhaha56-maker/ddddddd/refs/heads/main/data.txt"
 
 Clear-Host
-Write-Host "=============================="
-$UserKey = Read-Host "Enter License Key"
-Write-Host "=============================="
+Write-Host "==============================" -ForegroundColor White
+$UserKey = Read-Host "  Enter License Key "
+Write-Host "==============================" -ForegroundColor White
 
 $DeviceUUID = (Get-CimInstance Win32_ComputerSystemProduct).UUID
+
+Write-Host "Verifying..." -ForegroundColor Cyan
+
 $FullUrl = $AppScriptUrl + "?key=" + $UserKey + "&hwid=" + $DeviceUUID
 
-Write-Host "Checking..."
-$Response = Invoke-RestMethod -Uri $FullUrl -Method Get
+try {
+    $Response = Invoke-RestMethod -Uri $FullUrl -Method Get
+} catch {
+    Write-Host "Error: Cannot connect to Server!" -ForegroundColor Red
+    pause
+    exit
+}
 
 if ($Response -eq "success") {
     Write-Host "Access Granted!" -ForegroundColor Green
-    $Base64String = Invoke-RestMethod -Uri $GitHubRawUrl
-    $Bytes = [System.Convert]::FromBase64String($Base64String)
-    $TempPath = "$env:TEMP\sys_cache.dll"
-    [System.IO.File]::WriteAllBytes($TempPath, $Bytes)
-    Write-Host "DLL Loaded. Cleaning up..."
-    Start-Sleep -Seconds 2
-    if (Test-Path $TempPath) { Remove-Item $TempPath -Force }
+    
+    try {
+        $Base64String = Invoke-RestMethod -Uri $GitHubRawUrl
+        
+        $Bytes = [System.Convert]::FromBase64String($Base64String)
+        $TempPath = "$env:TEMP\sys_cache_$(Get-Random).dll"
+        [System.IO.File]::WriteAllBytes($TempPath, $Bytes)
+
+        Write-Host "Loading AimbotFemaleFix..." -ForegroundColor Yellow
+
+        Start-Sleep -Seconds 3 
+        if (Test-Path $TempPath) {
+            Remove-Item -Path $TempPath -Force
+            Write-Host "Cleanup completed. System is Secured." -ForegroundColor Gray
+        }
+    } catch {
+        Write-Host "Error: Failed to process DLL data!" -ForegroundColor Red
+    }
 } else {
-    Write-Host "Access Denied!" -ForegroundColor Red
+    Write-Host "Access Denied! Check your Key or HWID." -ForegroundColor Red
     pause
 }
